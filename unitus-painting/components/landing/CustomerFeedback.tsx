fca51f682bda153db5d5593a2368dfb7a605beaa
soke14 <joshsoke@gmail.com>
2024-11-07 07:59:23 -0700

    Update CustomerFeedback.tsx

--- unitus-painting/components/landing/CustomerFeedback.tsx
+++ unitus-painting/components/landing/CustomerFeedback.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import { motion, useAnimation, AnimatePresence } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
